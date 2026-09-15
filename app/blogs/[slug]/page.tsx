e044bca1a072f6130739832d17f6711c288df5eb
asm-saim <saim777m@gmail.com>
2026-09-15 20:40:21 +0600

    dynamic blog displayed

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -1,7 +1,62 @@
-const page = () => {
+interface ParamsProps {
+  params: Promise<{
+    slug: string;
+  }>;
+}
+
+const data = [
+  {
+    id: 1,
+    name: "React",
+    category: "Frontend",
+    level: "Beginner",
+  },
+  {
+    id: 2,
+    name: "Node.js",
+    category: "Backend",
+    level: "Intermediate",
+  },
+  {
+    id: 3,
+    name: "MongoDB",
+    category: "Database",
+    level: "Intermediate",
+  },
+  {
+    id: 4,
+    name: "TypeScript",
+    category: "Language",
+    level: "Advanced",
+  },
+  {
+    id: 5,
+    name: "Tailwind CSS",
+    category: "Styling",
+    level: "Beginner",
+  },
+  {
+    id: 6,
+    name: "Docker",
+    category: "DevOps",
+    level: "Advanced",
+  },
+];
+const page = async ({ params }: ParamsProps) => {
+  const { slug } = await params;
+
+  const info = data.find((value) => value.id === parseInt(slug));
+
   return (
     <div>
-      <h1>This is dynamic part</h1>
+      <h1>ID No: {slug}</h1>
+      {info && (
+        <div>
+          <h1>{info.name}</h1>
+          <h1>{info.level}</h1>
+          <h1>{info.category}</h1>
+        </div>
+      )}
     </div>
   );
 };
--- app/components/Value.tsx
+++ app/components/Value.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface Data {
   id: number;
   name: string;
@@ -9,22 +11,19 @@ interface Props {
 }
 
 const Value = ({ value }: Props) => {
+  const { name, id } = value;
   return (
     <div>
       <div className="card bg-base-100 w-96 shadow-sm">
-        <figure>
-          <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
-        </figure>
         <div className="card-body">
           <h2 className="card-title">
-            {value.name}
+            {name}
             <div className="badge badge-secondary">NEW</div>
           </h2>
           <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-          <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
-          </div>
+          <Link href={`/blogs/${id}`}>
+            <button className="btn">See Detail</button>
+          </Link>
         </div>
       </div>
     </div>
