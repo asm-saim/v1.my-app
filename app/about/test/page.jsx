e17741784470e4b8f737946d3c0e7f79da48598b
asm-saim <saim777m@gmail.com>
2026-09-15 08:35:44 +0600

    created pages, routed in next js

---
+++ app/about/test/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>this is page section</div>;
+};
+
+export default page;
---
+++ app/developer/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>I am confident developer</h1>
+    </div>
+  );
+};
+
+export default Page;
