d12b579c9f2e4395b8785d61ab10f12400c8db51
asm-saim <saim777m@gmail.com>
2026-09-16 16:30:15 +0600

    image load from remote location

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,6 +5,17 @@ const Page = () => {
     <div>
       <Image src="/next.svg" alt="next img" width={500} height={500}></Image>
       <Image src="/GettyImages.jpg" alt="image of getty" width={500} height={300}></Image>
+
+      {/* Remote image load/outside img load  */}
+      <Image
+        src="https://images.unsplash.com/photo-1789167871825-dbfb0745c067"
+        alt="circle img"
+        width={500}
+        height={500}
+      ></Image>
+
+      {/* Form img bb */}
+      <Image src="https://i.ibb.co.com/7jsy44g/img-12.jpg" alt="jar img" width={500} height={500}></Image>
     </div>
   );
 };
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // https://images.unsplash.com/photo-1789167871825-dbfb0745c067
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "**",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co.com",
+        port: "",
+        pathname: "/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
