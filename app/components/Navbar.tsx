6c35ee3be389dd0fa37fc256d976c9516d9feb33
asm-saim <saim777m@gmail.com>
2026-09-16 07:59:45 +0600

    linking page,layout creation for specific folder(recaped)

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -15,6 +15,9 @@ const Navbar = () => {
       <li>
         <Link href="/blogs">Blogs</Link>
       </li>
+      <li>
+        <Link href="/dashboard">Dashboard</Link>
+      </li>
     </>
   );
 
